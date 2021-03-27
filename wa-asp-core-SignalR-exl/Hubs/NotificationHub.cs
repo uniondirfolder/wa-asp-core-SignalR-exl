fbd058a82f9e3013b6984c744e5b07a98a91e204
NVV <uniondirfolder@gmail.com>
2021-03-27 02:43:53 -0700

    Add project files.

---
+++ wa-asp-core-SignalR-exl/Hubs/NotificationHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace wa_asp_core_SignalR_exl.Hubs
+{
+    public class NotificationHub:Hub
+    {
+        public Task SendMessage(string message) 
+        {
+            return Clients.Others.SendAsync("Send", message);
+        }
+    }
+}
---
+++ wa-asp-core-SignalR-exl/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace wa_asp_core_SignalR_exl.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
