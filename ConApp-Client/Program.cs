6441b57287657e111e4c3f1af3844f322f76f699
NVV <uniondirfolder@gmail.com>
2021-03-27 03:29:09 -0700

    part 1

---
+++ ConApp-Client/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace ConApp_Client
+{
+    class Program
+    {
+        static HubConnection hubConnection; 
+        static async Task Main(string[] args)
+        {
+            hubConnection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:17910/notification")
+                .Build();
+
+            hubConnection.On<string>("Send", message => Console.WriteLine($"Message from server: {message}"));
+
+            await hubConnection.StartAsync();
+
+            bool isExit = false;
+
+            while (!isExit)
+            {
+                var message = Console.ReadLine();
+
+                if (message != "exit")
+                    await hubConnection.SendAsync("SendMessage", message);
+                else
+                    isExit = true;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
