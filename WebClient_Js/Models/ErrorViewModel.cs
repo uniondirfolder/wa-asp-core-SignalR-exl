5bd4f282059e544a367f53c38ed91ee9e9525e02
NVV <uniondirfolder@gmail.com>
2021-03-29 01:48:42 -0700

    Start part-3

---
+++ WebClient_Js/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebClient_Js.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
